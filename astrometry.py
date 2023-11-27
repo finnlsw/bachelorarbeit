2b1b78e94aa177af8fedc421366c57708e2c141d
finnlsw <finn.mahnken@stud.uni-heidelberg.de>
2023-11-27 10:35:20 +0100

    test

--- astrometry.py
+++ astrometry.py
@@ -3,20 +3,27 @@ import os
 from astroquery.astrometry_net import AstrometryNet
 from astropy.io import fits
 
-path = "/home/finn/visual_Studio_Code/data/2023-09-25/stacked/"
-input_files = glob.glob(os.path.join(path, '*.fit')) 
+path = "/home/finn/visual_Studio_Code/data/2023-09-30/stacked/"
 
-ast = AstrometryNet()
-ast.api_key = 'fpcodtulvgmteoid' # own key XXXXXX change at the end for next person
+def plate_solve(path):
+    input_files = glob.glob(os.path.join(path, '*.fit')) 
+    ast = AstrometryNet()
+    ast.api_key = 'fpcodtulvgmteoid'  # XXXXXXXXXXXXXXXXX change personal api key at end for other persons XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-for i,file in enumerate (input_files):
-    # detect sources and use source list to obtain wcs information from astronomy.net (add ,force_image_upload=True to upload image instead of use source list (is slower))
-    wcs_header = ast.solve_from_image(file) # additional params: FWHM, detect_threshold
-
-    # add wcs information to header
-    hdul = fits.open(file, mode='update')
-    hdul[0].header.extend(wcs_header, useblanks=True)
-    hdul.flush() #write changes to disk
-    hdul.close()
-    print(f"success for image {i+1}")
-    print("")
+    for i, file in enumerate(input_files):
+        try:
+            wcs_header = ast.solve_from_image(file) #determines and upload source list to plate solve 
+            if wcs_header is None or len(wcs_header) == 0: 
+                wcs_header = ast.solve_from_image(file, force_image_upload=True) # if its not working upload image (=slower)
+                print(f"Image {i+1} solved using force_image_upload")
+            else:
+                print(f"Image {i+1} solved using source list")
+            
+            hdul = fits.open(file, mode='update')
+            hdul[0].header.extend(wcs_header, useblanks=True)
+            hdul.flush() #write changes to disk
+            hdul.close()
+        except Exception as e:
+            print(f"Error processing image {i+1}: {str(e)}")
+    print("finished for all images")
+plate_solve(path)
